9b16387a4d58ccd7d9ab808b190b3217ee36a162
Zhang <skyxxzc@gmail.com>
2018-09-06 10:41:21 +0800

    output

--- gen_img.py
+++ gen_img.py
@@ -3,6 +3,18 @@ from PIL import Image
 import sys
 
 
+def emit_dot(cmd, img):
+    pinv = 0.5
+    def topos(x,y):
+        return (pinv*x, pinv*(img.size[1]-y-1))
+    for y in range(img.size[1]):
+        for x in range(img.size[0]) if y%2==0 else range(img.size[0]-1, -1, -1):
+            if img.getpixel((x,y)) == 0:
+                print(x,y, img.getpixel((x,y)))
+                cmd.do(CMD_UPPEN)
+                cmd.do(CMD_MOVETO, pos=topos(x,y))
+                cmd.do(CMD_DOWNPEN)
+    code.do(CMD_UPPEN)
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
@@ -17,18 +29,6 @@ if __name__ == '__main__':
     print(img.format, img.size, img.mode)
 
     code = Commands()
-
-    pinv = 0.5
-    def topos(x,y):
-        return (pinv*x, pinv*(img.size[1]-y-1))
-    for y in range(img.size[1]):
-        for x in range(img.size[0]) if y%2==0 else range(img.size[0]-1, -1, -1):
-            if px[x,y] == 0:
-                #print(x,y, px[x,y])
-                code.do(CMD_UPPEN)
-                code.do(CMD_MOVETO, pos=topos(x,y))
-                code.do(CMD_DOWNPEN)
-
-    code.do(CMD_UPPEN)
+    emit_dot(code, img)
     code.do(CMD_MOVETO, pos=(0,0))
     code.save(fncode)
