ec45958e2485627933bf4894fcb551442ba731f5
Zhang <skyxxzc@gmail.com>
2018-09-06 15:28:28 +0800

    linedraw

--- gen_img.py
+++ gen_img.py
@@ -16,6 +16,37 @@ def emit_dot(cmd, img):
                 cmd.do(CMD_DOWNPEN)
     code.do(CMD_UPPEN)
 
+def emit_line(cmd, img):
+    pinv = 0.5
+    def topos(x,y):
+        return (pinv*x, pinv*(img.size[1]-y-1))
+    xmax = img.size[0]
+
+    def line_beg():
+        cmd.do(CMD_MOVETO, pos=pos)
+        cmd.do(CMD_DOWNPEN)
+    def line_end():
+        cmd.do(CMD_MOVETO, pos=pos)
+        cmd.do(CMD_UPPEN)
+
+    for y in range(img.size[1]):
+        pqueue = range(xmax) if y%2==0 else range(xmax-1, -1, -1)
+        for i,x in enumerate(pqueue):
+            if img.getpixel((x,y)) == 0:
+                pos = topos(x,y)
+                if i==0:
+                    line_beg()
+                    break
+                if(i == xmax -1):
+                    line_end()
+                    break
+                if(img.getpixel((pqueue[i-1],y)) > 0):
+                    line_beg()
+                    break
+                if(img.getpixel((pqueue[i+1],y)) > 0):
+                    line_end()
+                    break
+
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         fnimg = sys.argv[1]
